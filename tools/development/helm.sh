6f98a1c6f55c7e895e2a859a37c22b22255f613f
Othon Tomoutzoglou <otto_sta@hotmail.com>
2025-02-17 12:41:36 +0200

    Update development helm script

--- tools/development/helm.sh
+++ tools/development/helm.sh
@@ -3,21 +3,26 @@
 # Check that there are at least 3 arguments (username, version, at least one component)
 if [[ "$#" -lt 3 ]]; then
     echo "Syntaxt error: insufficient arguments."
-    echo "Use: $0 DOCKER_USERNAME VERSIONE component [component2 ...]"
+    echo "Use: $0 DOCKER_USERNAME VERSIONE EDGE component [component2 ...]"
+    echo "EDGE values: \"on\", \"off\" and refers to provider cluster edge IoT support"
     exit 1
 fi
 
 DOCKER_USERNAME="$1"
 VERSION="$2"
-
-# Remove the first two arguments (username and version) to handle only components and images
-shift 2
+EDGE="$3"
+# Remove the first three arguments (username and version) to handle only components and images
+shift 3
 
 # Associative array to associate components to their corresponding Helm values
 declare -A COMPONENT_MAP
 COMPONENT_MAP["rear-controller"]="rearController.imageName"
 COMPONENT_MAP["rear-manager"]="rearManager.imageName"
 COMPONENT_MAP["local-resource-manager"]="localResourceManager.imageName"
+COMPONENT_MAP["network-manager"]="networkManager.imageName"
+if [ $EDGE == "on" ]; then
+    COMPONENT_MAP["edge-resource-manager"]="edgeResourceManager.imageName"
+fi
 
 # Initialize a variable to store the --set options
 IMAGE_SET_STRING=""
@@ -35,18 +40,11 @@ for component in "$@"; do
     IMAGE_SET_STRING="$IMAGE_SET_STRING --set $helm_key=$DOCKER_USERNAME/$component"
 done
 
-export KUBECONFIG=../../testbed/kind/consumer/config
+export KUBECONFIG=../scripts/fluidos-consumer-1-config
 
 # Delete all the resources
-kubectl delete solvers.nodecore.fluidos.eu -n fluidos --all
-kubectl delete discoveries.advertisement.fluidos.eu -n fluidos --all
-kubectl delete reservations.reservation.fluidos.eu -n fluidos --all
-kubectl delete contracts.reservation.fluidos.eu -n fluidos --all
-kubectl delete peeringcandidates.advertisement.fluidos.eu -n fluidos --all
-kubectl delete transactions.reservation.fluidos.eu -n fluidos --all
-kubectl delete allocations.nodecore.fluidos.eu -n fluidos --all
-kubectl delete flavours.nodecore.fluidos.eu -n fluidos --all
-
+kubectl delete -f ../../deployments/node/crds
+# Install all the resources
 kubectl apply -f ../../deployments/node/crds
 
 # Run the helm command
@@ -55,18 +53,15 @@ helm uninstall node -n fluidos
 eval "helm install node ../../deployments/node -n fluidos -f consumer_values.yaml $IMAGE_SET_STRING" --set tag="$VERSION"
 rm consumer_values.yaml
 
-export KUBECONFIG=../../testbed/kind/provider/config
+if [ $EDGE == "on" ]; then
+    export KUBECONFIG=../scripts/fluidos-provider-1-edge-config
+else
+    export KUBECONFIG=../scripts/fluidos-provider-1-config
+fi
 
 # Delete all the resources
-kubectl delete solvers.nodecore.fluidos.eu -n fluidos --all
-kubectl delete discoveries.advertisement.fluidos.eu -n fluidos --all
-kubectl delete reservations.reservation.fluidos.eu -n fluidos --all
-kubectl delete contracts.reservation.fluidos.eu -n fluidos --all
-kubectl delete peeringcandidates.advertisement.fluidos.eu -n fluidos --all
-kubectl delete transactions.reservation.fluidos.eu -n fluidos --all
-kubectl delete allocations.nodecore.fluidos.eu -n fluidos --all
-kubectl delete flavours.nodecore.fluidos.eu -n fluidos --all
-
+kubectl delete -f ../../deployments/node/crds
+# Install all the resources
 kubectl apply -f ../../deployments/node/crds
 
 # Run the helm command
