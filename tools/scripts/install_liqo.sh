091b022918d739b94538e4c82df2b80b4bb4e9d9
Othon Tomoutzoglou <otto_sta@hotmail.com>
2025-03-13 19:29:45 +0200

    Fix: explicitly define last compatible liqo version. Remove duplicate liqo install command

--- tools/scripts/install_liqo.sh
+++ tools/scripts/install_liqo.sh
@@ -31,11 +31,13 @@ CLUSTER_NAME=$2
 # Get the Kubeconfig
 KUBECONFIG_LIQO=$3
 
+# Get Liqo version
+LIQO_VER=$4
+
 helm repo update
 
 # Install Liqo based on the provider
-liqoctl install "$PROVIDER" --cluster-name "$CLUSTER_NAME" --kubeconfig "$KUBECONFIG_LIQO" || { echo "Failed to install Liqo for provider: $PROVIDER"; exit 1; }
-liqoctl install "$PROVIDER" --cluster-name "$CLUSTER_NAME" --kubeconfig "$KUBECONFIG_LIQO" || { echo "Failed to install Liqo for provider: $PROVIDER"; exit 1; }
+liqoctl install "$PROVIDER" --cluster-name "$CLUSTER_NAME" --kubeconfig "$KUBECONFIG_LIQO" --version "$LIQO_VER" || { echo "Failed to install Liqo for provider: $PROVIDER"; exit 1; }
 # liqoctl install "$PROVIDER" || { echo "Failed to install Liqo for provider: $PROVIDER"; exit 1; }
 
 echo "Liqo installation for provider $PROVIDER completed successfully."
